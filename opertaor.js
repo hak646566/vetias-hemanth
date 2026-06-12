97ea6e09e51443d8c18978bc87c50e2092f1b81e
hak646566 <hak646566@gmail.com>
2026-06-12 11:41:19 +0530

    operator.js

---
+++ opertaor.js
@@ -0,0 +1,9 @@
+//Arithmetic
+var a = 10;
+var b = 20;
+console.log(a+b); // addition
+console.log(a-b); // subtrction
+console.log(a*B); //multiplication
+console.log(a/b); // division
+console.log(a%b); //modulas
+console.log(a**b); //exponention
