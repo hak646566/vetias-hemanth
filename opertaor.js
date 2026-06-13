3eefb3e76907710180100a5d77037ee8ed742e1f
hak646566 <hak646566@gmail.com>
2026-06-13 13:46:58 +0530

    added looping.js

--- opertaor.js
+++ opertaor.js
@@ -1,9 +1,18 @@
-//Arithmetic
-var a = 10;
-var b = 20;
-console.log(a+b); // addition
-console.log(a-b); // subtrction
-console.log(a*B); //multiplication
-console.log(a/b); // division
-console.log(a%b); //modulas
-console.log(a**b); //exponention
+// Arithmetic operators
+let x=10;
+let y=5;
+console.log(x+y);
+console.log(x-y);
+console.log(x/y);
+console.log(x*y);
+console.log(x%y);
+//ternary operator
+let age = 18;
+let canvote= (age>=18) ? "yes" :"no";
+
+
+console.log(canvote);
+//boolean operator
+let age = 17;
+let canvote =(age.=18) ? "true" : "false"
+console.log(canvote)
---
+++ sata.js
@@ -0,0 +1,6 @@
+//no parametic functions
+function greet() sole.log(
+    "Fav actor=Ajith kumar,Fav player=mike tyson,Fav movie=mahaan");
+}
+greet(); // output: 
+
